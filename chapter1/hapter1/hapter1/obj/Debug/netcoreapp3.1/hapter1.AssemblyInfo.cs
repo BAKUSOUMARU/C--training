4ad2073118d2d4c7ffba0818a98291038112febf
BAKUSOUMARU <88494642+BAKUSOUMARU@users.noreply.github.com>
2022-03-28 01:17:20 +0900

    修正

--- chapter1/hapter1/hapter1/obj/Debug/netcoreapp3.1/hapter1.AssemblyInfo.cs
+++ chapter1/hapter1/hapter1/obj/Debug/netcoreapp3.1/hapter1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("chapter1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("hapter1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("chapter1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("chapter1")]
+[assembly: System.Reflection.AssemblyProductAttribute("hapter1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("hapter1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment クラスによって生成されました。
